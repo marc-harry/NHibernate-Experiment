c4074405ae1a9ddce5efd332f8d3937f2b528468
Marc Harry <mjharry1@gmail.com>
2014-04-07 10:31:02 +0100

    First commit

---
+++ NHibernate.DataAccess/Domain/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernate.DataAccess.Domain
+{
+    public interface IProductRepository
+    {
+        void Add(Product product);
+        void Update(Product product);
+        void Remove(Product product);
+        Product GetById(Guid productId);
+        Product GetByName(string name);
+        ICollection<Product> GetByCategory(string category);
+    }
+}
---
+++ NHibernate.Test/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NHibernate.DataAccess.Repositories;
+using NHibernate.DataAccess.Domain;
+using NHibernate.DataAccess.Repositories;
+
+namespace NHibernate.Test
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                IList<Product> products = session.CreateCriteria<Product>().List<Product>();
+
+                foreach (Product product in products)
+                {
+                    productList.InnerHtml += string.Format("<li>Product name: {0} <br />Product type: {1} </li>", product.Name, product.Category);
+                }                
+            }
+        }
+    }
+}
