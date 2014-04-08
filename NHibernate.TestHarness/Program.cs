40b3462f4b3ea5893f3fb7b97a8900453fd58bb3
Marc Harry <mjharry1@gmail.com>
2014-04-08 09:23:29 +0100

    Added test harness project

---
+++ NHibernate.TestHarness/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.DataAccess.Domain;
+using NHibernate.DataAccess.Repositories;
+
+namespace NHibernate.TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                IList<Product> products = session.CreateCriteria<Product>().List<Product>();
+
+                foreach (Product product in products)
+                {
+                    Console.WriteLine("Product name: {0} \nProduct type: {1} \n \n", product.Name, product.Category);
+                }
+
+                Console.Read();
+            }
+        }
+    }
+}
